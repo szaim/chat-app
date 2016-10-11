7290fc733d86977d0b1d33b9f791838d899413aa
szaim <zaimsofia81@gmail.com>
2016-10-11 19:19:48 -0400

    create a function to get the URL parameter

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,9 @@
 var socket = io(); 
 
+var name = getUrlParameter('name');;
+
+console.log("name", name);
+
 $("form").submit(function() {
   socket.emit("chatMessage", $("#m").val());
   $("#m").val('');
