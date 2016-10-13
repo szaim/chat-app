2a06c497c74628213183d7ce505b8c890e9f105e
szaim <zaimsofia81@gmail.com>
2016-10-12 22:02:35 -0400

    refactoring query params

--- public/js/queryparams.js
+++ public/js/queryparams.js
@@ -1,6 +1,14 @@
-function getUrlParameter(name) {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-    var results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-};
+function getUrlParameter(key) {
+	var search = location.search.replace("?", "").split("&");
+	var value;
+	search.forEach(function(item) {
+		item = item.split("=");
+		if(item[0] == key) {
+			value = item[1];
+			return false;
+		}
+	});
+
+	return value;
+
+}
